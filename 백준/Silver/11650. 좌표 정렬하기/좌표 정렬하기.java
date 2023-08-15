7f3916f13f28c6c2638b861298c023aaff8d8c54
Youth787 <wkdus885@gmail.com>
2023-08-15 14:45:08 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 752 ms, Memory: 69560 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		int[][] dot = new int[T][2];
+		for (int i = 0; i < T; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 2; j++) {
+				dot[i][j] = Integer.parseInt(st.nextToken());
+			}
+		} // 입력받기
+
+		// 2차월 배열 정렬 0번쨰 열 다음 1번째 열 기준(다중 배열 정렬)
+		Arrays.sort(dot, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				if (o1[0] == o2[0]) {
+					return o1[1] - o2[1];
+				} else {
+					return o1[0] - o2[0];
+				}
+			}
+		});
+
+		StringBuilder sb = new StringBuilder();
+		for(int i = 0 ; i< T ; i++) {
+			sb.append(dot[i][0] + " " + dot[i][1]).append('\n');
+		}
+		System.out.println(sb);
+		
+	}//main end 
+}
