3f17cdcaedd9a83efce7288a3466beeb58236990
Youth787 <wkdus885@gmail.com>
2023-08-27 11:05:56 +0900

    Delete 프로그래머스_/Level1/덧칠하기 directory

--- 프로그래머스_/Level1/덧칠하기/덧칠하기.py
+++
@@ -1,9 +0,0 @@
-def solution(n, m, section):
-    result = 1
-    loc = section[0]+m-1
-    for i in range(1,len(section)):
-        if section[i] > loc:
-            loc = section[i]+m-1
-            result += 1
-        
-    return result
