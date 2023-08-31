3bf7822821f1f0662c3a2a1ba7719b23a769077b
Youth787 <wkdus885@gmail.com>
2023-08-31 17:05:51 +0900

    [D2] Title: 백만 개의 정수 정렬, Time: 479 ms, Memory: 111,348 KB -BaekjoonHub

--- SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
+++ SWEA/D2/14229. 백만 개의 정수 정렬/백만 개의 정수 정렬.java
@@ -4,55 +4,54 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-	 static int n;
-	static int[] sortedArr,array;
+	static int n;
+	static int[] array;
 
 	public static void main(String[] args) throws IOException {
-		// 병합정렬
+		// 퀵정렬 - 호어파티션
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n=1000000;
+		StringTokenizer st;
+		n = 1000000;
 		array = new int[n];
-		sortedArr = new int[n];
-		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
 			array[i] = Integer.parseInt(st.nextToken());
 		} // 입력받기 완료
 
-		mergeSort(0, n-1);
+		quickSort(0, n - 1);
 		System.out.print(array[500000]);
 
 	}// main end
 
-	public static void mergeSort(int left, int right) {
-		if (left < right) {
-			int mid = (left + right) / 2;
-			mergeSort(left, mid);
-			mergeSort(mid + 1, right);
-			merge(left, mid, right);
+	public static void quickSort(int l, int r) {
+		if (l < r) {
+			int pivot = Partition(l, r);
+			quickSort(l, pivot - 1);
+			quickSort(pivot + 1, r);
 		}
-	}// mergesort method end
+	}
 
-	public static void merge(int left, int mid, int right) {
-		int L = left;
-		int R = mid + 1;
-		int idx = left;
-
-		while (L <= mid && R <= right) {
-			if (array[L] <= array[R])
-				sortedArr[idx++] = array[L++];
-			else
-				sortedArr[idx++] = array[R++];
-		} // while end
-
-		if (L <= mid) {
-			for (int i = L; i <= mid; i++)
-				sortedArr[idx++] = array[i];
-		} else {
-			for (int j = R; j <= right; j++)
-				sortedArr[idx++] = array[j];
+	public static int Partition(int left, int right) {
+		int pivot = array[left];
+		int L = left + 1;
+		int R = right;
+		while (L <= R) {
+			while (L <= R && array[L] <= pivot)
+				L++;
+			while (array[R] > pivot)
+				R--;
+			if (L < R) {
+				swap(array,L,R);
+			}
 		}
-		for (int i = left; i <= right; i++) {
-			array[i] = sortedArr[i];
-		}
+		swap(array,left,R);
+		return R;
+	}
+
+	public static void swap(int[] array , int a, int b) {
+		int tmp = array[a];
+		array[a] = array[b];
+		array[b] = tmp;
 	}
 }
