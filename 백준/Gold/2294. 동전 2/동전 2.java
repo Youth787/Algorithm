1704849a8db6987c6db1fc1ba97ee6e03dfd0766
Youth787 <wkdus885@gmail.com>
2023-09-13 20:08:08 +0900

    [Gold V] Title: 동전 2, Time: 92 ms, Memory: 11724 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -4,6 +4,10 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// dp 문제는 어렵다. 
+// 30분동안 고민해도 답이 안나온다면 
+// 여러가지 풀이를 보고 많은 문제를 풀어보며 dp를 푸는 법에 익숙해지는것이 좋다.
+
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,7 +18,7 @@ public class Main {
 		int[] money = new int[n];
 		int[] dp = new int[k+1];
 		
-		Arrays.fill(dp, 100001);
+		Arrays.fill(dp, 987654321);
 		dp[0] = 0;
 		
 		for(int i =0; i<n; i++) {
@@ -26,6 +30,6 @@ public class Main {
 				dp[j] = Math.min(dp[j], dp[j-money[i]]+1); 
 			}
 		}
-		System.out.println(dp[k]==100001? -1:dp[k]);
+		System.out.println(dp[k]==987654321? -1:dp[k]);
 	}// main end 
 }
