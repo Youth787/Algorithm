701a9cdf32447795843146563aa38fdce1b6c22c
Youth787 <wkdus885@gmail.com>
2023-09-25 19:15:49 +0900

    [Bronze I] Title: 세로읽기, Time: 80 ms, Memory: 11488 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[][] arr = new int[5][15];
+		
+		for(int i=0; i<5; i++) {
+			String s = br.readLine();
+			for(int j=0; j<s.length(); j++) {
+				arr[i][j] = s.charAt(j); 
+			}
+		}// 입력받기 완료 
+		
+//		for(int i =0; i<5; i++) {
+//			for(int j =0; j<15; j++) {
+//				System.out.print(arr[i][j]+" ");
+//			}
+//			System.out.println();
+//		} // 제대로 입력 받음 
+		
+		for(int i=0; i<15; i++) {
+			for(int j=0; j<5; j++) 
+				if(arr[j][i]==0) 
+					continue;
+				else System.out.print((char)arr[j][i]);
+		}
+	}
+}
