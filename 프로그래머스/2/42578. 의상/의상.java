8c5b691449d52763ed4aef7cd881e0c6822cabaa
Youth787 <wkdus885@gmail.com>
2023-12-11 20:10:05 +0900

    [level 2] Title: 의상, Time: 0.07 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,19 +1,31 @@
 import java.util.*;
 
+class clothes {
+    String name;
+    int cnt;
+    
+    public clothes(String name, int cnt){
+        this.name = name;
+        this.cnt = cnt;
+    }
+} 
+
     class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<clothes.length; i++){
-            String key = clothes[i][1];
-            if(!map.containsKey(key)) map.put(key,1);
-            else map.put(key,map.get(key)+1);
+        List<clothes> list = new LinkedList<>();
+        out : for(int i=0; i<clothes.length; i++){
+            for(int j=0; j<list.size(); j++){
+                if(list.size() !=0 && list.get(j).name.equals(clothes[i][1])) {
+                    list.get(j).cnt++; continue out;}
+            }
+            list.add(new clothes(clothes[i][1],1));
         }
-        
-        Iterator<Integer> it = map.values().iterator();
-        while(it.hasNext()){
-            answer *= it.next().intValue() +1;
+				      
+        if(list.size()==1){return clothes.length;}
+        else{
+            int result =1;
+            for(int i =0; i<list.size(); i++) result *= list.get(i).cnt;
+            return clothes.length+result;
         }
-        return answer -1;
     }
 }
