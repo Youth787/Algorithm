ef6c73003731fc0d25c1bc499cbff21d04bfa682
Youth787 <wkdus885@gmail.com>
2023-12-14 15:23:02 +0900

    [level 2] Title: 의상, Time: 0.36 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,31 +1,19 @@
 import java.util.*;
-
-class clothes {
-    String name;
-    int cnt;
-    
-    public clothes(String name, int cnt){
-        this.name = name;
-        this.cnt = cnt;
-    }
-} 
-
-    class Solution {
+class Solution {
     public int solution(String[][] clothes) {
-        List<clothes> list = new LinkedList<>();
-        out : for(int i=0; i<clothes.length; i++){
-            for(int j=0; j<list.size(); j++){
-                if(list.size() !=0 && list.get(j).name.equals(clothes[i][1])) {
-                    list.get(j).cnt++; continue out;}
-            }
-            list.add(new clothes(clothes[i][1],1));
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], 1);
         }
-				      
-        if(list.size()==1){return clothes.length;}
-        else{
-            int result =1;
-            for(int i =0; i<list.size(); i++) result *= list.get(i).cnt;
-            return clothes.length+result;
+        for (int i = 0; i < clothes.length; i++) {
+            map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
         }
+        
+        System.out.println(map);
+        for (int count : map.values()) {
+            answer *= count;
+        }
+        return answer - 1;
     }
 }
