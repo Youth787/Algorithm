d75e080fea8db2813fe6bfc40f57b2fdc2425ac4
Youth787 <wkdus885@gmail.com>
2023-12-27 22:01:23 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 704 ms, Memory: 295536 KB -BaekjoonHub

---
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[][] dir = new int[][]{{1,0},{0,1},{-1,0},{0,-1}};
+    static int[][] dist;
+    static int[][] arr;
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+       int testcase =0;
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        while(true){
+            testcase++;
+            N = Integer.parseInt(br.readLine());
+            if(N==0) break;
+
+            arr = new int[N][N];
+            dist = new int[N][N];
+            for(int i=0; i<N; i++) Arrays.fill(dist[i],Integer.MAX_VALUE);
+
+            for(int i=0; i<N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for(int j=0; j<N; j++)
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+
+            BFS();
+            // for(int[] a : dist) System.out.println(Arrays.toString(a));
+            sb.append("Problem "+testcase+": "+dist[N-1][N-1]+"\n");
+        }
+        System.out.println(sb);
+    }
+    public static void BFS(){
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0,arr[0][0]});
+
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int i = curr[0];
+            int j = curr[1];
+            int cost = curr[2];
+
+            // dist[i][j] = Math.min(dist[i][j], cost+arr[i][j]);
+
+            for(int k=0; k<4; k++){
+                int r = i+dir[k][0];
+                int c = j+dir[k][1];
+                if(r>=0 && r<N && c>=0 && c<N){
+                    if(dist[r][c]> cost+arr[r][c]){
+                        dist[r][c] = cost+arr[r][c];
+                        q.offer(new int[]{r,c,cost+arr[r][c]});
+                    }
+                }
+            }
+        }
+    }
+}
+
