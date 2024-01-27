44d19fe7e12ed5a326feb73562aa086698a8ead3
Youth787 <wkdus885@gmail.com>
2024-01-27 17:40:22 +0900

    [level 2] Title: 피로도, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,41 +1,19 @@
-import java.util.*;
-
 class Solution {
     int ans =0;
-    int[][] arr;
-    int N;
-    int K;
-    int[] result;
     boolean[] visit;
     public int solution(int k, int[][] dungeons) {
-        arr= dungeons;
-        N = dungeons.length;
-        K = k;
-        result = new int[N];
-        visit = new boolean[N];
-        npn(0);
+        visit = new boolean[dungeons.length];
+        npn(k, dungeons, 0);
         return ans;
     }
-    public void npn(int idx){
-        if(idx==N){
-            int P = K;
-            int cnt =0;
-            for(int a : result){
-                if(P>=arr[a][0]){
-                    P -= arr[a][1];
-                    cnt++;
-                } else break;
-            }            
-            ans = Math.max(ans,cnt);
-            return;
+    public void npn(int tired, int[][] dungeons, int cnt){
+        for(int i=0; i<dungeons.length; i++){
+            if(!visit[i] && tired>=dungeons[i][0]) {
+                visit[i] = true;
+                npn(tired-dungeons[i][1], dungeons, cnt+1);
+                visit[i] = false;
+            }
         }
-        
-        for(int i=0; i<N; i++){
-            if(visit[i]) continue;
-            visit[i] = true;
-            result[idx] = i;
-            npn(idx+1);
-            visit[i] = false;
-        }
+        ans = Math.max(ans,cnt);
     }
 }
